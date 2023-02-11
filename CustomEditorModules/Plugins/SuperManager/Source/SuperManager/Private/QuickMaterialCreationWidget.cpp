8f7ab87806573d69dc8a17f9c5081f72470da516
George Huan <george.huan1994@foxmail.com>
2023-02-12 02:44:26 +0800

    Create QuickMaterialCreation editor widget

---
+++ CustomEditorModules/Plugins/SuperManager/Source/SuperManager/Private/QuickMaterialCreationWidget.cpp
@@ -0,0 +1,14 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "QuickMaterialCreationWidget.h"
+#include "DebugHeader.h"
+
+#pragma region QuickMaterialCreationCore
+
+void UQuickMaterialCreationWidget::CreateMaterialFromSelectedTextures()
+{
+	Debug::ShowNotifyInfo(TEXT("Working"));
+}
+
+#pragma endregion
